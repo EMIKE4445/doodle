01e5673488bf4e3a6fe792811e6d9ab5c05bc3ad
Michael Emmanuel <EmmanuelOjonugwa009@gmail.com>
2021-06-08 14:07:14 +0100

    uddated game

--- application.js
+++ application.js
@@ -1,13 +1,17 @@
 const ball= document.getElementById('ball');
 const container= document.getElementById('container');
 const score=document.getElementById('score-number');
+const ground= document.querySelector('#ground');
 let current_score=0;
+
+const obstacleClass= ['obstacle', 'obstacle2'];
 
 
 let gravity=0.9;
 
 // instatiating balls position
-let ball_position= 40;
+let ball_position= 200;
+let obstacleHeight=200;
 let is_jumping= false;
 
 //time variable used in villan interval
@@ -28,22 +32,18 @@ function controller(event){
 
 
 function jump(){
-    
-    
-   
+
    let timer= setInterval(()=>{
-        if(ball_position>200){
+        if(ball_position>350){
             clearInterval(timer);
-            
 
             let down_interval= setInterval(()=>{
-                if(ball_position<=40){
+                if(ball_position==210){
                     clearInterval(down_interval);
                     is_jumping=false;
 
                 }
                 ball_position-= 10;
-                
                 ball.style.bottom =ball_position +'px';
             },20);
         }
@@ -64,11 +64,10 @@ function game_over(){
     document.getElementById('game-over').style.display='block';
 }
 
-function create_random_obstacle(obstacle_type){
-    // obstacle types  are rectangles on ground and flat tiles above ground
+function create_random_obstacle(index){
 
     let obstacle= document.createElement('div');
-    obstacle.classList.add('obstacle');
+    obstacle.classList.add(obstacleClass[index]);
     return obstacle;
 }
 
@@ -86,14 +85,22 @@ document.getElementById('retry').addEventListener('click',()=>{
 
 function villan(){
     console.log(time);
-   let obstacle= create_random_obstacle();
+    let index=0;
+    obstacleHeight=200;
+    if(current_score> 15){
+        index= Math.round(Math.random());
+        obstacleHeight= index==0 ? 200 : 300;
+    }
+
+    
+   let obstacle= create_random_obstacle(index);
    document.getElementById('game-objects').appendChild(obstacle);
    
    let position=1000;
    
 
    let timer =setInterval(()=>{
-        if((position<=70) && (ball_position<50)){
+        if(((position<=70) && (ball_position<250)) ||( (ball_position > 280) && (obstacleHeight==300) && (position <=70) ) ){
             clearInterval(timer)
             
             clearInterval(villan_interval);
@@ -122,6 +129,7 @@ function villan(){
     },20);
 
 }
+
 
 
 
